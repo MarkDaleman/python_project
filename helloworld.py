e6c396605342aedc059f3450dfa0ab35abefca3f
Mark Daleman <markdaleman@gmail.com>
2015-12-02 14:17:30 +0100

    - Database is gevuld met Data en de Main is leeg

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import matplotlib.pyplot as plt
 import tweepy
 from tweepy import OAuthHandler
 
@@ -12,37 +12,52 @@ consumer_secret = "7OIv98yOr4Ep3vCJpR3XdSMW3wcDfGURbvWvVmwwuBKg6KIE7C"
 
 
 def getTweets():
-    maxTweets = 10000 # Some arbitrary large number
-    searchQuery = 'Nederland'  # this is what we're searching for
-    print("Tabel wordt nu gevuld met Twitter berichten")
+    # Achterlijk groot getal
+    maxTweets = 10000
+    # Op welke hashtag gaan we zoeken
+    searchQuery = 'Nederland'
+    print("Table is being filled now. With tweets")
     auth = OAuthHandler(consumer_key, consumer_secret)
-
+    # Zorgen dat de API kan verbinden
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
+    # Python laten verbinden met de Sqlite database
     conn = sqlite3.connect('tweets.db')
     c = conn.cursor()
-
-
     searched_tweets = [status for status in tweepy.Cursor(api.search, q=searchQuery).items(maxTweets)]
     for tweet in searched_tweets:
         voorbeeld = (tweet.text)
         c.execute('INSERT INTO tweets VALUES(?)', (voorbeeld,))
+    # Database wijzigingen opslaan
     conn.commit()
-    print("Database is geupdatet met alle tweets")
+    print("Database is up to date! ^.^")
 
 
 def clearTabel():
+    # Python laten verbinden met de Sqlite database en leegmaken
     conn = sqlite3.connect('tweets.db')
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS tweets")
     c.execute("CREATE TABLE tweets (text TEXT);")
-    print("Tabel tweets is leeg en nieuwe is aangemaakt")
+    print("Table tweet is dropped and created a new one")
+
+def countTweets():
+    conn = sqlite3.connect('tweets.db')
+    print ("Opened database successfully");
+    cursor = conn.execute("SELECT COUNT(*) FROM tweets");
+    for row in cursor:
+        print ("Aantal Tweets = ", row[0])
+    print ("Operation done successfully, closing connection");
+    conn.close();
+
+def drawSomething():
+    plt.plot([1,2,3,4])
+    plt.ylabel('some numbers')
+    plt.show()
 
 if __name__ == '__main__':
-
-print("I'm working!")
-
+    countTweets();
+    drawSomething();
 
 
 
