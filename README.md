0c9ee14a654a7f73f373da014352c62c2c99dc99
Mark Daleman <markdaleman@gmail.com>
2015-12-02 11:13:11 +0100

    - Database is gevuld met Data en de Main is leeg

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# python_project_markdaleman
-Python Project Periode 3 Mark Daleman
+# python twitter moodanalyse applicatie
+Python Project Periode 2 Mark Daleman ISCP
+
+Nodig om tweets op te halen:
+ try:
+        clearTabel()
+        getTweets()
+    except Exception as exC:
+        print('I just caught the exception: %s' % exC)
--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
 import sqlite3
+
 import tweepy
-from time import sleep
-from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
+
 
 #Variables that contains the user credentials to access Twitter API
 access_token = "579994863-7kUav4vqVupLe45MQdhCXhu8Y2d7sXq3ut739BvN"
@@ -11,45 +11,41 @@ consumer_key = "BXwPemXe8UB9cVEDmymRjawre"
 consumer_secret = "7OIv98yOr4Ep3vCJpR3XdSMW3wcDfGURbvWvVmwwuBKg6KIE7C"
 
 
-class StdOutListener(StreamListener):
-    def on_data(self, data):
-        print("Ik krijg data terug!")
-        return True
-
-    def on_error(self, status):
-        print(status)
-
-if __name__ == '__main__':
-    l = StdOutListener()
+def getTweets():
+    maxTweets = 10000 # Some arbitrary large number
+    searchQuery = 'Nederland'  # this is what we're searching for
+    print("Tabel wordt nu gevuld met Twitter berichten")
     auth = OAuthHandler(consumer_key, consumer_secret)
+
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    query = 'Germany'
-    max_tweets = 10000
-    searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
-
-    ## DB connectie openen
     conn = sqlite3.connect('tweets.db')
     c = conn.cursor()
-    ## Drop table als al bestaat, en maak daarna een nieuwe aan
-    c.execute("DROP TABLE IF EXISTS tweets")
-    c.execute("CREATE TABLE tweets (text TEXT);")
-    ## Counter op 0 zetten zodat ik straks een sleep kan gebruiken
-    count = 0
 
+
+    searched_tweets = [status for status in tweepy.Cursor(api.search, q=searchQuery).items(maxTweets)]
     for tweet in searched_tweets:
-        ## Zo genereren dat er alleen text in het veld komt
         voorbeeld = (tweet.text)
-        ## Knal de data naar de database
         c.execute('INSERT INTO tweets VALUES(?)', (voorbeeld,))
-        #Counter
-        count += 1
-        if count >= 500:
-            print("Sleeping.. Zz")
-            sleep(30)
-            count = 0
     conn.commit()
     print("Database is geupdatet met alle tweets")
+
+
+def clearTabel():
+    conn = sqlite3.connect('tweets.db')
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS tweets")
+    c.execute("CREATE TABLE tweets (text TEXT);")
+    print("Tabel tweets is leeg en nieuwe is aangemaakt")
+
+if __name__ == '__main__':
+
+print("I'm working!")
+
+
+
+
+
 
 
