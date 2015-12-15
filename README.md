4d07e1da5c2e4848dfd3c98db5a9e99362a21969
Mark Daleman <markdaleman@gmail.com>
2015-12-15 12:18:02 +0100

    - Edited Readme - Code working - Can draw a graph now - Working database - Switched to using TextBlob (see readme)

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# python twitter moodanalyse applicatie
+# Twitter Mood analyse
 Python Project Periode 2 Mark Daleman ISCP
 
-Nodig om tweets op te halen:
- try:
-        clearTabel()
-        getTweets()
-    except Exception as exC:
-        print('I just caught the exception: %s' % exC)
+
+# Wat heb ik gebruikt ?
+
+- Python 3.4
+- Tweepy (http://www.tweepy.org)
+- Matplotlib (http://matplotlib.org)
+- TextBlob (https://textblob.readthedocs.org/en/dev/#)
+- sqlite 3 (https://docs.python.org/2/library/sqlite3.html) 
--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,9 @@ import sqlite3
 import matplotlib.pyplot as plt
 import tweepy
 from tweepy import OAuthHandler
+from textblob import TextBlob
+import numpy as np
+
 
 
 #Variables that contains the user credentials to access Twitter API
@@ -13,10 +16,9 @@ consumer_secret = "7OIv98yOr4Ep3vCJpR3XdSMW3wcDfGURbvWvVmwwuBKg6KIE7C"
 
 def getTweets():
     # Achterlijk groot getal
-    maxTweets = 10000
+    maxTweets = 500
     # Op welke hashtag gaan we zoeken
-    searchQuery = 'SanBernadino'
-    print("Table is being filled now. With tweets")
+    searchQuery = 'happy'
     auth = OAuthHandler(consumer_key, consumer_secret)
     # Zorgen dat de API kan verbinden
     auth.set_access_token(access_token, access_token_secret)
@@ -24,59 +26,81 @@ def getTweets():
     # Python laten verbinden met de Sqlite database
     conn = sqlite3.connect('engels.db')
     c = conn.cursor()
-    searched_tweets = [status for status in tweepy.Cursor(api.search, q=searchQuery).items(maxTweets)]
+    searched_tweets = [status for status in tweepy.Cursor(api.search, q=searchQuery, language="en").items(maxTweets)]
     for tweet in searched_tweets:
-        voorbeeld = (tweet.text)
-        c.execute('INSERT INTO tweets VALUES(?)', (voorbeeld,))
+        tweets = (tweet.text)
+        c.execute('INSERT INTO tweets VALUES(?)', (tweets,))
     # Database wijzigingen opslaan
     conn.commit()
-    print("Database is up to date! ^.^")
+    print("Database is up to date met de laatste tweets")
 
-def oneTweet():
-    maxTweets = 2
-    searchQuery = 'france'
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    searched_tweets = [status for status in tweepy.Cursor(api.search, q=searchQuery).items(maxTweets)]
-    for (tweet) in searched_tweets:
-        print(searched_tweets)
-        print("Text van de Tweet")
-        print(tweet.text)
-        print("Timezone van de Tweet")
-        print(tweet.user.time_zone)
-        print("Screen name")
-        print(tweet.user.screen_name)
-        print("name")
-        print(tweet.user.name)
-        print("\n")
-
-
-
-def clearTabel():
+def clearTweetTabel():
     # Python laten verbinden met de Sqlite database en leegmaken
-    conn = sqlite3.connect('tweets.db')
+    conn = sqlite3.connect('engels.db')
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS tweets")
     c.execute("CREATE TABLE tweets (text TEXT);")
-    print("Table tweet is dropped and created a new one")
+    print("Tweet tabel is verwijderd en opnieuw aangemaakt")
 
-def countTweets():
-    conn = sqlite3.connect('tweets.db')
-    print ("Opened database successfully");
-    cursor = conn.execute("SELECT COUNT(*) FROM tweets");
-    for row in cursor:
-        print ("Aantal Tweets = ", row[0])
-    print ("Operation done successfully, closing connection");
-    conn.close();
+def clearAnalyseTabel():
+    # Python laten verbinden met de Sqlite database en leegmaken
+    conn = sqlite3.connect('engels.db')
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS analyse")
+    c.execute("CREATE TABLE analyse (analyse NUMERIC);")
+    print("analyse tabel is verwijderd en opnieuw aangemaakt")
 
-def drawSomething():
-    plt.plot([1,2,3,4])
-    plt.ylabel('some numbers')
+def analyseTweets():
+    print("Ik ga nu analyseren")
+    conn = sqlite3.connect('engels.db')
+    c = conn.cursor()
+
+    tweetDatabase = conn.execute("SELECT * from tweets");
+    for row in tweetDatabase:
+        alleTweets = TextBlob(row[0])
+
+        ## DEBUG
+        # print(alleTweets.sentiment.polarity)
+
+        c.execute("INSERT INTO analyse VALUES(?)", (alleTweets.sentiment.polarity,))
+        conn.commit()
+    print("Database is up to date met analysegegevens")
+    tweetAll = conn.execute("SELECT COUNT(*) FROM analyse")
+    for row in tweetAll:
+        tweetAll = row
+
+    tweetNegatief = conn.execute("SELECT COUNT(*) FROM analyse WHERE analyse < 0.0")
+    for row in tweetNegatief:
+        tweetNegatief = row
+
+    tweetPositief = conn.execute("SELECT COUNT(*) FROM analyse WHERE analyse > 0.0")
+    for row in tweetPositief:
+        tweetPositief = row
+
+    tweetNeutraal = conn.execute("SELECT COUNT(*) FROM analyse WHERE analyse = 0.0")
+    for row in tweetNeutraal:
+        tweetNeutraal = row
+
+    # DEBUG PROCENTEN
+    #print (int((tweetNeutraal[0] * 100) / tweetAll[0])) #aantal Procent wat Neutraal is
+    #print (int((tweetPositief[0] * 100) / tweetAll[0])) #aantal Procent wat Positief is
+    #print (int((tweetNegatief[0] * 100) / tweetAll[0])) #aantal Procent wat Negatief is
+
+    # Pie chart weergeven met de waardes van de tweets.
+    labels = 'Neutraal', 'Positief', 'Negatief'
+    sizes = [(int((tweetNeutraal[0] * 100) / tweetAll[0])), (int((tweetPositief[0] * 100) / tweetAll[0])), (int((tweetNegatief[0] * 100) / tweetAll[0]))]
+    colors = ['yellow', 'green', 'red']
+    explode = (0, 0, 0)
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+            autopct='%1.1f%%', shadow=True, startangle=90)
+    # Set aspect ratio to be equal so that pie is drawn as a circle.
+    plt.axis('equal')
     plt.show()
-
 if __name__ == '__main__':
-    oneTweet()
+    clearAnalyseTabel()
+    clearTweetTabel()
+    getTweets()
+    analyseTweets()
 
 
 
